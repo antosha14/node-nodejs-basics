26e77f1bc94090586d5cb5f9120fda69f759368c
unknown <anton.kozel.97@mail.ru>
2024-10-02 11:36:56 +0300

    feat: write create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { open, writeFile } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = './files/fresh.txt';
+  const textContent = 'I am fresh and young';
+  const errorMessage = 'FS operation failed';
+
+  open(filePath, 'wx', (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        throw new Error(errorMessage);
+      }
+      throw err;
+    }
+
+    writeFile(fd, textContent, err => {
+      if (err) throw err;
+    });
+  });
 };
 
 await create();
