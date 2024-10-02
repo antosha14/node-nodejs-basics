92ea2e0160d2faf15ff919da61912207e3b0f4dc
unknown <anton.kozel.97@mail.ru>
2024-10-02 18:10:11 +0300

    feat: complete module task and hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs';
+
+const pathToFile = './files/fileToCalculateHashFor.txt';
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    readFile(
+      pathToFile,
+      {
+        encoding: 'utf-8',
+      },
+      (err, data) => {
+        if (err) throw err;
+        console.log(createHash('sha256').update(data).digest('hex'));
+      }
+    );
+  } catch (err) {}
 };
 
 await calculateHash();
