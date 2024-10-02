72a2dff688f6193628dea6867759d84745b75e9c
unknown <anton.kozel.97@mail.ru>
2024-10-02 16:29:05 +0300

    feat: do cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,13 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argumentsArray = process.argv.reduce((accumulator, arg, index, array) => {
+    if (arg.startsWith('--')) {
+      const argValue = `${arg.slice(2)} is ${array[index + 1]}`;
+      return [...accumulator, argValue];
+    }
+    return accumulator;
+  }, []);
+
+  console.log(argumentsArray.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envVariablesArray = Object.entries(process.env).reduce((accumulator, [key, value]) => {
+    if (key.startsWith('RSS_')) {
+      const envVariable = `${key}=${value}`;
+      return [...accumulator, envVariable];
+    }
+    return accumulator;
+  }, []);
+
+  console.log(envVariablesArray.join('; '));
 };
 
 parseEnv();
