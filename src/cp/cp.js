1421342fde1aec9da0ccd2caceb95400fecd0b69
unknown <anton.kozel.97@mail.ru>
2024-10-03 13:08:47 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { fork } from 'child_process';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const scriptLocation = join(__dirname, './files/script.js');
+
+const spawnChildProcess = async args => {
+  fork(scriptLocation, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess();
