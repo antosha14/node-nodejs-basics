c5e21f41b4065e11c3ff9caac4466a00b37c35dd
unknown <anton.kozel.97@mail.ru>
2024-10-03 11:47:02 +0300

    feat: implement working threads task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+
+const cpuCoreQuantity = cpus().length;
+const workerFile = './src/wt/worker.js';
+
+const results = [];
+
 const performCalculations = async () => {
-    // Write your code here
+  const workers = [];
+  for (let i = 0; i < cpuCoreQuantity; i++) {
+    const worker = new Worker(workerFile, { workerData: i + 10 });
+    workers.push(worker);
+
+    worker.on('message', message => {
+      results.push({ status: 'resolved', data: message });
+    });
+
+    worker.on('error', error => {
+      results.push({ status: 'error', data: null });
+    });
+  }
+
+  Promise.all(
+    workers.map(
+      worker =>
+        new Promise(resolve => {
+          worker.on('message', resolve);
+          worker.on('error', resolve);
+        })
+    )
+  ).then(() => {
+    console.log(results);
+  });
 };
 
 await performCalculations();
